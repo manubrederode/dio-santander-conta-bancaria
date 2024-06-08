f1ad01f80e62bf506caab85dc50d1c9ea9322b66
Manu Brederode <brederode.emmanuelle@gmail.com>
2024-06-08 17:35:20 +0100

    Add files via upload

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) throws Exception {
+        
+        //Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        //Attributes and Inputs
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
